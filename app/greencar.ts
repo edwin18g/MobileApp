65395a223237a330a4aa090b5a9e4ad8a755a314
edwin18g <edwin18g@gmail.com>
2017-12-17 20:35:33 +0530

    Create greencar.ts

---
+++ app/greencar.ts
@@ -0,0 +1,8 @@
+export class GreenCar {
+    constructor(
+        public greenCarId: number,
+        public greenCarName: string,
+        public greenCarColor: string,
+        public greenCarDoors: number
+    ) {  }
+}
